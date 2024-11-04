0b8343b77ac2fa669c687177c54fde25e3ece07f
T Burton <tyler.burton@gmail.com>
2024-11-04 15:35:41 -0700

    pushing versin 9

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2591332520"
+    serviceWorkerVersion: "1433916997"
   }
 });
